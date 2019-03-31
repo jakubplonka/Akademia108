5036ab7f7e887c10f8fbc0749f74c4caa65d2bcd
Jakub <slomkas@gmail.com>
2019-03-31 15:00:44 +0200

    filtrowanie

--- zajecia3103/crypto-app/src/Crypto/Crypto.js
+++ zajecia3103/crypto-app/src/Crypto/Crypto.js
@@ -6,34 +6,61 @@ class Crypto extends Component {
   constructor() {
     super();
     this.state = {
-      currency: []
+      currency: [],
+      filteredValues: []
     }
   }
+  
+  
 
-  componentDidMount(prevProps, prevState) {
+  insertData(){
     axios.get(`https://blockchain.info/pl/ticker`)
       .then(res => {
         const currency = res.data;
-        if (currency !== prevState){
-          this.setState({ currency });
-        }
+        const currencyArray = Object.keys(currency).map( keys => {
+          let convertedCurrencyArray = {};
+          convertedCurrencyArray.currency = keys;
+          convertedCurrencyArray.last = currency[keys].last;
+          convertedCurrencyArray.buy = currency[keys].buy;
+          convertedCurrencyArray.sell = currency[keys].sell;
+          convertedCurrencyArray.symbol = currency[keys].symbol;
+          return convertedCurrencyArray;
+        })
+        this.setState({ currency: currencyArray });
       })
+  }
+
+  componentDidMount() {
+    this.insertData();
     setInterval(() => {
-      axios.get(`https://blockchain.info/pl/ticker`)
-      .then(res => {
-        const currency = res.data;
-        if (currency !== prevState){
-          this.setState({ currency });
-        }
-      })
+      this.insertData();
     }, 5000);
   }
 
+  handleChange = () => {
+    const {value} = this.filterValue;
+    const filteredList = this.state.currency.filter( el => {
+      return el.currency.toLowerCase().includes(value);
+    })
+    if (filteredList === 0 && value){
+      this.setState({
+        filteredValues: [{ currency: 'No currency found', key: Date.now() }]
+      })
+      return
+    }
+    if (value === ''){
+      this.setState({filteredValues: []});
+      return
+    }
+    this.setState({ filteredValues: filteredList });
+  }
+
   render() {
+    const {currency, filteredValues} = this.state;
     return (
       <div className="currencyList">
-      <input onChange={this.handleChange}/>
-        <CryptoList currencyList={this.state.currency}/>
+      <input type="text" onChange={this.handleChange} ref={data => this.filterValue = data}/>
+        <CryptoList currencyList={filteredValues.length > 0 ? filteredValues : currency}/>
       </div>
     )
   }
--- zajecia3103/crypto-app/src/CryptoList/CryptoList.js
+++ zajecia3103/crypto-app/src/CryptoList/CryptoList.js
@@ -2,20 +2,12 @@ import React from 'react';
 import './CryptoList.css'
 
 const CryptoList = props => {
-  const currencyArray = Object.keys(props.currencyList).map( keys => {
-    let convertedCurrencyArray = {};
-    convertedCurrencyArray.currency = keys;
-    convertedCurrencyArray.last = props.currencyList[keys].last;
-    convertedCurrencyArray.buy = props.currencyList[keys].buy;
-    convertedCurrencyArray.sell = props.currencyList[keys].sell;
-    convertedCurrencyArray.symbol = props.currencyList[keys].symbol;
-    return convertedCurrencyArray;
-  })
+  
 
     return (
       <>
       <ul>
-        { currencyArray.map(cur => (
+        { props.currencyList.map(cur => (
           <li key={cur.currency} className="currency"><span>Last rate:</span> {cur.last}  <span>{cur.currency} [{cur.symbol}]</span></li>
         ))}
       </ul>
